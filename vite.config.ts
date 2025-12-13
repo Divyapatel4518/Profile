ec4f28fc5d03a16d30311c7821166ab24accfe5c
Divya Patel <Divyapatel4518@gmail.com>
2025-12-13 11:57:13 +1000

    Normalize line endings

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: "/Profile/", 
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './'),
       },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'nprocess.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    }
+  };
 });
-
 
