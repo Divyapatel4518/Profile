3c290d5a20901a2bf2aa65d21fe773f1ff329fd9
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-13 12:11:44 +1000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Tailwind import
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
