0c78fb85132b3e2b292e91057ded09f54541087c
Divyapatel4518 <Divya.patel4518@gmail.com>
2025-12-13 13:50:27 +1000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Tailwind import
+import "./index.css";
 
+// Find root element
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Could not find root element to mount to");
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
+// Create React root
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
